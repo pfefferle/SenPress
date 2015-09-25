50fcd56425f9bbb3fbaab6a7e8b7c55d0beb3ab9
pfefferle <pfefferle@gmail.com>
2015-09-26 00:00:58 +0200

    moved old child theme

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 900; /* pixels */
+}
+
+function senpress_setup() {
+	// This theme uses post thumbnails
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 900, 1288 ); // Unlimited height, soft crop
+
+	// Register custom image size for image post formats.
+	add_image_size( 'sempress-image-post', 900, 1288 );
+
+	// remove some theme supports from SemPress
+	remove_theme_support( 'custom-background' );
+	remove_theme_support( 'custom-header' );
+
+	// remove some unneeded actions
+	remove_action( 'wp_head', 'sempress_customize_css' );
+	remove_action( 'customize_register', 'sempress_customize_register' );
+}
+/**
+ * Tell WordPress to run sempress_setup() when the 'after_setup_theme' hook is run.
+ */
+add_action( 'after_setup_theme', 'senpress_setup', 12 );
---
+++ readme.md
@@ -0,0 +1,9 @@
+# SenPress
+
+Pure Zen for [SemPress](https://github.com/pfefferle/SemPress)!
+
+![Screenshot](screenshot.png)
+
+(inspired by the nice and simple [SemPress Lite](https://github.com/jihaisse/SemPress-Lite) child theme by @Jihaisse)
+
+If you look for a real Zen theme, have a look at [ZenPress](https://github.com/pfefferle/ZenPress)!
